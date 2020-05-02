c68ea79d63025b41df7ebd9ef4b5dd6494715804
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-05-02 10:04:39 +0700

    Revert "sched: Improve the scheduler"
    reverts commit 1be1a8c0
    reverts commit 7ec7f761
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4904,10 +4904,8 @@ unsigned int sched_lib_mask_force;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
-	char *libname, *lib_list;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
-	char tmp_lib_name[LIB_PATH_LENGTH];
 	bool found = false;
 	struct task_struct *p;
 	struct mm_struct *mm;
@@ -4939,15 +4937,10 @@ bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
-			strlcpy(tmp_lib_name, sched_lib_name, LIB_PATH_LENGTH);
-			lib_list = tmp_lib_name;
-			while ((libname = strsep(&lib_list, ","))) {
-				libname = skip_spaces(libname);
-				if (strnstr(name, libname,
+			if (strnstr(name, sched_lib_name,
 					strnlen(name, LIB_PATH_LENGTH))) {
-					found = true;
-					goto release_sem;
-				}
+				found = true;
+				break;
 			}
 		}
 	}
